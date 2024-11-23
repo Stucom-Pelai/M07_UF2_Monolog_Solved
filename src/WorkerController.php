0196c2b268454fb4bc4a86f03e0ef0515992c43c
Japoivvb <jose.portugal@stucom.com>
2024-11-23 11:21:33 +0100

    first commit

---
+++ src/WorkerController.php
@@ -0,0 +1,48 @@
+<?php
+$steps=0;
+// load dependencies
+require '../vendor/autoload.php'; //FIXME
+++$steps;
+use Monolog\Level;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create log
+$log = new Logger("LogWorkerDB");
+// define logs location
+$log->pushHandler(new StreamHandler("../logs/WorkerDB.log", Level::Info)); //FIXME
+++$steps;
+
+//ddbb connection, read from miConf.ini
+//TODO
+$db = parse_ini_file("../conf/miConf.ini"); 
+++$steps;
+
+try {
+    $mysqli = new mysqli($db["host"], $db["user"], $db["pwd"], $db["db_name"]); //4 db
+    // write info message with "Connection successfully"
+    //TODO
+    $log->info("Connection successfully");
+    ++$steps;
+
+    // Create operation
+    $sql_sentence = "INSERT INTO worker(dni,name,surname,salary,phone) 
+            VALUES('71111111D','Juan','González',20000,'93500202')";
+
+    try {
+        $result = $mysqli->query($sql_sentence);
+        // write info message with "Record inserted successfully"
+        //TODO
+        $log->info("Record inserted successfully");
+        ++$steps;
+    } catch (mysqli_sql_exception $e) {
+        //  write error message with "Error inserting a record"
+        //TODO
+        $log->error("Error inserting a record");
+    }
+} catch (mysqli_sql_exception $e) {
+    //  write error message with "Error connection db: + details parameters config"
+    //TODO
+    $log->error("Error connection db: " . json_encode($db));
+}
+echo "steps executed correctly: " . $steps;
---
+++ vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array(
+    'root' => array(
+        'name' => 'uf2/joseportugal',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'monolog/monolog' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e94000419394ff1bec801dad310432228f9fc19c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../monolog/monolog',
+            'aliases' => array(
+                0 => '3.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'psr/log' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f16e1d5863e37f8d8c2a01719f5b34baa2b714d3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/log',
+            'aliases' => array(
+                0 => '3.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'psr/log-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '3.0.0',
+            ),
+        ),
+        'uf2/joseportugal' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
